0c94cfc09cd178c2ccff8a48411ca029a7a9969e
arif-uddin <arif.uddin.cse@gmail.com>
2018-09-18 21:29:30 +0600

    Dashboard Added

---
+++ Online Exam System/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Exam_System.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
