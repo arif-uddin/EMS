7b7030d7d1a6b6ada53382632b456affcc58a761
Akash Khan <shahadat4024@diu.edu.bd>
2018-09-29 14:37:58 +0600

    lookup cs added

---
+++ Models/ViewModels/TrainerVM/ParticipantAddVm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Models.ViewModels.TrainerVM
+{
+   public class ParticipantAddVm
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string RegNo { get; set; }
+        public string ContactNo { get; set; }
+        public string Email { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public int PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Profession { get; set; }
+        public string HighestAcademicQualification { get; set; }
+        public byte Image { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public int BatchId { get; set; }
+        public Batch Batch { get; set; }
+
+        public List<SelectListItem> BatchLookup { get; set; }
+        public List<SelectListItem> CourseLookup { get; set; }
+        public List<SelectListItem> OrganizationLookup { get; set; }    
+
+    }
+}
--- Online Exam System/Models/LookUp.cs
+++ Online Exam System/Models/LookUp.cs
@@ -10,11 +10,23 @@ namespace Online_Exam_System.Models
     public class LookUp
     {
         OrganizationManager _organizationManager = new OrganizationManager();
+        CourseManager _courseManager = new CourseManager();
+        BatchManager _batchManager = new BatchManager();
 
         public List<SelectListItem> GetOrganizations()
         {
             return _organizationManager.GetAll().Select(
                 c => new SelectListItem() {Value = c.Id.ToString(), Text = c.Name}).ToList();
+        }
+        public List<SelectListItem> GetCourses()
+        {
+            return _courseManager.GetAll().Select(
+                c => new SelectListItem() { Value = c.Id.ToString(), Text = c.Name }).ToList();
+        }
+        public List<SelectListItem> GetBatches()
+        {
+            return _batchManager.GetAll().Select(
+                c => new SelectListItem() { Value = c.Id.ToString(), Text = c.BatchNo.ToString() }).ToList();
         }
 
     }
